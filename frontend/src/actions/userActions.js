f23555f1e227bafd35c6c3cdaa52239028054956
mriyank <mriyanksingh@gmail.com>
2021-07-15 14:29:00 +0530

    User Login Screen & Functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,42 +1,41 @@
-import { USER_LOGIN_SUCCESS, USER_LOGIN_FAIL } from '../constants/userConstants';
-import  axios from 'axios';
-export const login = (email,password) = async (dispatch) => {
-    try {
-        dispatch({
-type: USER_LOGIN_REQUEST
-        })
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+} from "../constants/userConstants";
+import axios from "axios";
 
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    });
 
-        const config = {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
+    const { data } = await axios.post(
+      "/api/users/login",
+      { email, password },
+      config
+    );
 
-        const {data} = await axios.post(
-            '/api/users/login',
-            {email,password},
-            config
-            )
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
 
-        dispatch({
-            type: USER_LOGIN_SUCCESS,
-            payload: data
-
-        })
-
-
-        localStorage.setItem('userInfo',Json.stringify(data))
-
-    } catch (error){
-        dispatch({
-            type: USER_LOGIN_FAIL,
-            payload:
-              error.response && error.response.data.message
-                ? error.response.data.message
-                : error.message,
-          });
-
-    }
-}
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
