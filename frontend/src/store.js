cd11e0372ca993185cba58191f3367ee933c0e8a
mriyank <mriyanksingh@gmail.com>
2021-07-16 16:12:42 +0530

    Shipping Screen & Save Address

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -33,8 +33,15 @@ const userInfoFromStorage = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
   : null;
 
+const shippingAddressfromStorage = localStorage.getItem("shippingAddress")
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
+  : {};
+
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressfromStorage,
+  },
   userLogin: { userInfo: userInfoFromStorage },
 };
 
